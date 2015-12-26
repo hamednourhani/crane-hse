0f10dbf6a9f735580609dfaedbdcb8045170b4a2
Hamed Nourhani <info@itstar.ir>
2015-12-26 17:23:09 +0330

    translation added

--- index.php
+++ index.php
@@ -1,44 +1,43 @@
 <?php get_header(); ?>
-	
-	<main class="site-main">
-		
-		
+
+	<main class="site-main archive-articles">
+
+
 		<div class="site-content">
 			<section class="layout">
-				
+
 				<div class="primary">
 
-			
-								<?php get_template_part('library/banner','maker'); ?>
-				
-						
+
 					<?php if(have_posts()){ ?>
 						<?php while(have_posts()) { the_post(); ?>
-						
+
 							<article class="hentry">
-								<div class="featured-image single-image">
-									<?php the_post_thumbnail(); ?>
-								</div>
-								<header class="article-title">
-									<a href="<?php the_permalink(); ?>">
-										<h3><?php the_title(); ?></h3>
-									</a>
-								</header>
-								<main class="article-body">
-									<div class="featured-image single-image">
+								<header class="article-header">
+
+									<div class="featured-image">
 										<a href="<?php the_permalink(); ?>">
-											<?php the_post_thumbnail(); ?>
+											<?php echo get_the_post_thumbnail();?>
 										</a>
 									</div>
-									<?php the_excerpt(); ?>
-									<?php get_template_part('library/post','meta'); ?>
+
+								</header>
+								<main class="article-body">
+									<div class="article-title">
+										<a href="<?php the_permalink(); ?>">
+											<h3><?php the_title(); ?></h3>
+										</a>
+									</div>
+									<?php the_excerpt();?>
 								</main>
 							</article>
 						<?php } ?>
-					<?php } ?>		
+					<?php } ?>
+
+
 					<nav class="pagination">
 						<?php crane_hse_pagination(); ?>
-					</nav>		
+					</nav>
 				</div><!-- primary -->
 
 				<div class="secondary">
@@ -46,7 +45,7 @@
 				</div><!-- secondary -->
 			</section>
 		</div>
-		
+
 	</main>
 
 <?php get_footer(); ?>
